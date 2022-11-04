1e2ad9d36e934009142dd627e0f348d336e218a0
Shivansh_Kumar <91822395+creator5112021@users.noreply.github.com>
2022-11-04 20:00:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function sound() {
+    window.location = "https://creator5112021.github.io/psychic-octo-disco/"
+}
+function paint() {
+    window.location = "https://creator5112021.github.io/project/"
+}
+function guest() {
+    window.location = "https://creator5112011.github.io/ghfyghhvjg/"
+}
+function me() {
+    window.location = "https://creator5112021.github.io/Project--78/"
+}
+function modelpol() {
+    window.location = "https://www.tinkercad.com/things/9WBqStpi5Ei"
+}
+function modelgsl() {
+    window.location = "https://www.tinkercad.com/things/h7MKYa9GueM"
+}
+function modelmac() {
+    window.location = "https://www.tinkercad.com/things/68ZpUSesbBw"
+}
